ab18ee616e514acde2903e2335a0fdb3eb2ae271
Daniel Griffiths <griffithsdanielc@gmail.com>
2016-05-02 18:38:30 -0700

    Room and User Models

--- app/src/main/java/com/rip/roomies/models/Room.java
+++ app/src/main/java/com/rip/roomies/models/Room.java
@@ -1,23 +1,28 @@
 package com.rip.roomies.models;
 
+import com.rip.roomies.sql.SQLCreate;
+import com.rip.roomies.sql.SQLFind;
+
 /**
  * Created by Kanurame on 5/2/2016.
  */
 public class Room {
+    String name = "";
+
     //------- CONSTRUCTORS -------//
 
     public Room(String name) {
-
+        this.name = name;
     }
 
     //------- DATABASE METHODS -------//
 
     public boolean createRoom() {
-        return false;
+        return SQLCreate.createRoom(this);
     }
 
     public boolean findRoom() {
-        return false;
+        return SQLFind.findRoom(this);
     }
 
     //------- OBJECT METHODS -------//
--- app/src/main/java/com/rip/roomies/models/User.java
+++ app/src/main/java/com/rip/roomies/models/User.java
@@ -1,39 +1,63 @@
 package com.rip.roomies.models;
 
+import com.rip.roomies.sql.SQLCreate;
+import com.rip.roomies.sql.SQLLogin;
+
 /**
  * Created by Kanurame on 4/25/2016.
  */
 public class User {
+    public int id = 0;
+    public String firstName = "";
+    public String lastName = "";
+    public String username = "";
+    public String email = "";
+    private String password = "";
+    public int room;
+
     //------- CONSTRUCTORS -------//
 
     public User(String username, String passwd) {
-
+        this.username = username;
+        this.password = passwd;
     }
 
     public User(int id, String username, String email) {
-
+        this.id = id;
+        this.username = username;
+        this.email = email;
     }
 
     public User(String firstName, String lastName, String username, String email,
                 String passwd) {
-
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = passwd;
     }
 
     //------- DATABASE METHODS -------//
 
     public boolean createUser() {
-        return false;
+        return SQLCreate.createUser(this);
     }
 
     public boolean login() {
-        return false;
+        if (SQLLogin.login(this)) {
+            this.password = "";
+            return true;
+        }
+        else
+            return false;
     }
 
     public boolean passRetrieve() {
-        return false;
+        return SQLLogin.passRetrieve(this);
     }
 
     public boolean update() {
+        //TODO
         return false;
     }
 
@@ -41,10 +65,14 @@ public class User {
 
     public void setFields(int id, String firstName, String lastName, String username,
                           String email) {
-
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
     }
 
     public void setRoom(int roomId) {
-
+        this.room = roomId;
     }
 }
