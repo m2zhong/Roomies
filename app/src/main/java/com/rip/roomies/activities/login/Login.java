b2c618b7270ec9b62874acad029ea48afd6ae547
Michael Zhong <m2zhong@ucsd.edu>
2016-05-08 15:54:35 -0700

    added button changes

--- app/src/main/java/com/rip/roomies/activities/login/Login.java
+++ app/src/main/java/com/rip/roomies/activities/login/Login.java
@@ -1,15 +1,32 @@
 package com.rip.roomies.activities.login;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.rip.roomies.R;
 import com.rip.roomies.activities.GenericActivity;
+import com.rip.roomies.events.login.createuser.SubmitListener;
+import com.rip.roomies.sql.SQLQuery;
+
+import java.sql.SQLException;
 
 public class Login extends GenericActivity {
+    Button login;
+    Button create_user;
+    EditText user_name;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+
+
+        try {
+            SQLQuery.execute("EXEC PROC CreateUser");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
