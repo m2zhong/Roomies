f5adc0ee234797cb1da0cc165f8f3f6b01a187c2
Daniel Griffiths <griffithsdanielc@gmail.com>
2016-05-08 22:52:53 -0700

    Changed access privileges, added Javadoc to Models

--- app/src/main/java/com/rip/roomies/models/Group.java
+++ app/src/main/java/com/rip/roomies/models/Group.java
@@ -7,20 +7,30 @@ import java.util.logging.Logger;
  * Created by Kanurame on 5/2/2016.
  */
 public class Group {
-    String name = "";
-    String description = "";
+    private String name = "";
+    private String description = "";
+
+    private static final Logger log = Logger.getLogger(Group.class.getName());
 
     //------- CONSTRUCTORS -------//
 
+    /**
+     * Constructor that creates a new Group.
+     * @param name The name of this Group.
+     * @param description The description of the Group.
+     */
     public Group(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
-    private static final Logger log = Logger.getLogger(Group.class.getName());
-
     //------- DATABASE METHODS -------//
 
+    /**
+     * Connects to the database, creating a new Group with the name and description used in this
+     * object's fields.
+     * @return The newly created Group.
+     */
     public Group createGroup() {
         return SQLCreate.createGroup(this);
     }
--- app/src/main/java/com/rip/roomies/models/User.java
+++ app/src/main/java/com/rip/roomies/models/User.java
@@ -8,34 +8,49 @@ import java.util.logging.Logger;
  * Created by Kanurame on 4/25/2016.
  */
 public class User {
-    public int id = 0;
-    public String firstName = "";
-    public String lastName = "";
-    public String username = "";
-    public String email = "";
+    private int id = 0;
+    private String firstName = "";
+    private String lastName = "";
+    private String username = "";
+    private String email = "";
     private String password = "";
-    public int room;
 
     private static User activeUser;
 	private static final Logger log = Logger.getLogger(User.class.getName());
 
-	public static User getActiveUser() {
-		return activeUser;
-	}
-
 	//------- CONSTRUCTORS -------//
 
+    /**
+     * Constructor used when logging in. Constructs the current active user using only the
+     * information provided at the Login prompt.
+     * @param username The User's login username.
+     * @param passwd The User's password, as entered on the login screen.
+     */
     public User(String username, String passwd) {
         this.username = username;
         this.password = passwd;
     }
 
+    /**
+     * Constructor used when retrieving a User's information from the database.
+     * @param id The unique ID identifying this User in the database.
+     * @param username The User's login username.
+     * @param email The email address used to contact this User.
+     */
     public User(int id, String username, String email) {
         this.id = id;
         this.username = username;
         this.email = email;
     }
 
+    /**
+     * Constructor used when creating a User. Populates all fields except id.
+     * @param firstName The User's first name.
+     * @param lastName The User's last name.
+     * @param username The User's login username.
+     * @param email The email address used to contact this User.
+     * @param passwd The User's password used to login.
+     */
     public User(String firstName, String lastName, String username, String email,
                 String passwd) {
         this.firstName = firstName;
@@ -47,15 +62,10 @@ public class User {
 
 	//------- DATABASE METHODS -------//
 
-    public User addToGroup() {
-        //TODO
-        return null;
-    }
-
-    public User createUser() {
-        return SQLCreate.createUser(this);
-    }
-
+    /**
+     * Connects to the database, logging in with information provided at the login prompt.
+     * @return The newly logged-in user.
+     */
     public User login() {
         User loggedIn = SQLLogin.login(this);
 
@@ -66,12 +76,41 @@ public class User {
         return loggedIn;
     }
 
+    /**
+     * Connects to the database, adding this User to an existing Group.
+     * @return The new User instance, with its Group set.
+     */
+    public User addToGroup() {
+        //TODO
+        return null;
+    }
+
+    /**
+     * Connects to the database, creating a new User with the information provided.
+     * @return The newly created User.
+     */
+    public User createUser() {
+        return SQLCreate.createUser(this);
+    }
+
+    /**
+     * Connects to the database and emails the User their password, which they have forgotten.
+     * @return true if the information used to retrieve the password was valid.
+     */
     public boolean passRetrieve() {
         return SQLLogin.passRetrieve(this);
     }
 
 	//------- OBJECT METHODS -------//
 
+    /**
+     * Unified mutator, setting all fields except for the password.
+     * @param id The unique ID identifying this User in the database.
+     * @param firstName The User's first name.
+     * @param lastName The User's last name.
+     * @param username The User's login username.
+     * @param email The email address used to contact this User.
+     */
     public void setFields(int id, String firstName, String lastName, String username,
                           String email) {
         this.id = id;
@@ -81,25 +120,12 @@ public class User {
         this.email = email;
     }
 
-    public void setRoom(int roomId) {
-        this.room = roomId;
-    }
-
-    public String getEmail() {
-        return email;
+    public static User getActiveUser() {
+        return activeUser;
     }
 
     public String getFirstName() {
         return firstName;
-    }
-
-    public int getGroupId() {
-        //TODO
-        return 0;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getLastName() {
@@ -110,7 +136,20 @@ public class User {
         return username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getGroupId() {
+        //TODO
+        return 0;
     }
 }
