8c58e314a95e092717b7102b1be4047c3b78fc69
Kanurame <zinorashi@gmail.com>
2016-05-08 21:46:05 -0700

    Formatted more files

--- app/src/main/java/com/rip/roomies/models/Group.java
+++ app/src/main/java/com/rip/roomies/models/Group.java
@@ -1,42 +1,43 @@
 package com.rip.roomies.models;
 
 import com.rip.roomies.sql.SQLCreate;
+
 import java.util.logging.Logger;
 
 /**
  * Created by Kanurame on 5/2/2016.
  */
 public class Group {
-    String name = "";
-    String description = "";
+	String name = "";
+	String description = "";
 
-    //------- CONSTRUCTORS -------//
+	//------- CONSTRUCTORS -------//
 
-    public Group(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+	public Group(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
 
-    private static final Logger log = Logger.getLogger(Group.class.getName());
+	private static final Logger log = Logger.getLogger(Group.class.getName());
 
-    //------- DATABASE METHODS -------//
+	//------- DATABASE METHODS -------//
 
-    public Group createGroup() {
-        return SQLCreate.createGroup(this);
-    }
+	public Group createGroup() {
+		return SQLCreate.createGroup(this);
+	}
 
-    //------- OBJECT METHODS -------//
+	//------- OBJECT METHODS -------//
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public int getId() {
-        //TODO
-        return 0;
-    }
+	public int getId() {
+		//TODO
+		return 0;
+	}
 }
