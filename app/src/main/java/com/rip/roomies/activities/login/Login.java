a13210e8013b4d9780bb365a20e400da93eb3862
Michael Zhong <m2zhong@ucsd.edu>
2016-05-09 12:28:38 -0700

    attempted to add listener but failed

--- app/src/main/java/com/rip/roomies/activities/login/Login.java
+++ app/src/main/java/com/rip/roomies/activities/login/Login.java
@@ -6,7 +6,8 @@ import android.widget.EditText;
 
 import com.rip.roomies.R;
 import com.rip.roomies.activities.GenericActivity;
-import com.rip.roomies.events.login.createuser.SubmitListener;
+import com.rip.roomies.events.login.CreateUserListener;
+import com.rip.roomies.events.login.LoginListener;
 import com.rip.roomies.sql.SQLQuery;
 
 import java.sql.SQLException;
@@ -14,32 +15,35 @@ import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class Login extends GenericActivity {
-<<<<<<< HEAD
-    Button login;
-    Button create_user;
-    EditText user_name;
-    EditText password;
+    private static final Logger log = Logger.getLogger(Login.class.getName());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button login;
+        Button create_user;
+        EditText user_name;
+        EditText password;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        user_name = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.loginbtn);
+        create_user = (Button) findViewById(R.id.regbtn);
+
+        login.setOnClickListener(new LoginListener(this,user_name,password));
 
         try {
             SQLQuery.execute("EXEC PROC CreateUser");
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-=======
-	private static final Logger log = Logger.getLogger(Login.class.getName());
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    protected void switch_regscreen(){
 
-		setContentView(R.layout.activity_login);
-	}
->>>>>>> e93fc9c26712ad29c4ae31baa6682dbdddc56c20
+
+
+    }
 }
--- app/src/main/java/com/rip/roomies/events/login/LoginListener.java
+++ app/src/main/java/com/rip/roomies/events/login/LoginListener.java
@@ -2,6 +2,7 @@ package com.rip.roomies.events.login;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.rip.roomies.activities.GenericActivity;
 
@@ -12,12 +13,40 @@ import java.util.logging.Logger;
  */
 public class LoginListener implements View.OnClickListener {
 	private static final Logger log = Logger.getLogger(LoginListener.class.getName());
+	private EditText username;
+	private EditText password;
+	private GenericActivity activity;
+
+
 
 	public LoginListener(GenericActivity context, EditText username, EditText passwd) {
-
+		this.username = username;
+		this.password = passwd;
+		this.activity = context;
 	}
 
 	public void onClick(View v) {
+		/*String Buffer for Error Message*/
+		StringBuffer errMessage = new StringBuffer();
+
+		/* Check if user entered username*/
+		if(username.getText().toString().isEmpty()) {
+			errMessage.append("Empty Username"+'\n');
+		}
+		/*Check if user entered password*/
+		if(password.getText().toString().isEmpty()) {
+			errMessage.append("Empty Password" + '\n');
+		}
+		/* Check if error occured*/
+		if(errMessage.length() != 0){
+			Toast.makeText(activity, errMessage, Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		/* Login Activity*/
+
+
+
 
 	}
 }
