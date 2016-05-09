faa2eda896f9106a9324ffdf31ae5d8650e86d52
Hao Tu <haotuusa@gmail.com>
2016-05-08 18:13:11 -0700

    CreateUser, CreateGroup, Login

--- app/src/main/java/com/rip/roomies/sql/SQLCreate.java
+++ app/src/main/java/com/rip/roomies/sql/SQLCreate.java
@@ -3,6 +3,7 @@ package com.rip.roomies.sql;
 import com.rip.roomies.models.Group;
 import com.rip.roomies.models.User;
 
+import java.sql.ResultSet;
 import java.util.logging.Logger;
 
 /**
@@ -12,10 +13,75 @@ public class SQLCreate {
 	private static final Logger log = Logger.getLogger(SQLCreate.class.getName());
 
 	public static Group createGroup(Group group) {
-		return null;
+
+		//declare the execution query code for TSQL to login
+		final String queryStr = "EXEC CreateGroup @name = '"+ group.getName() +
+				"', @description = '" + group.getDescription() + "'";
+
+		ResultSet rset = null;
+
+		try {
+			// get the result table from query execution through sql
+			rset = SQLQuery.execute(queryStr);
+
+			// group already exist
+			if(rset == null || rset.getRow() == 0) {
+				return null;
+			}
+			//if there's a rset
+			else {
+				rset.next();
+				//first column is id, second is lastname, third is firstname
+				//so pass the column number accordingly to get the info about user
+				String resultName = rset.getString(2);
+				String resultDescription = rset.getString(3);
+
+				return new Group(resultName, resultDescription);
+
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public static User createUser(User user) {
-		return null;
+
+		//declare the execution query code for TSQL to login
+		final String queryStr = "EXEC CreateUser @firstName = '" + user.getFirstName() +
+				"', @lastName = '" + user.getLastName() +
+				"', @username = '" + user.getUsername() +
+				"', @email= '" + user.getEmail() +
+				"', @password = '" + user.getPassword() + "'";
+
+		ResultSet rset = null;
+
+		try {
+			// get the result table from query execution through sql
+			rset = SQLQuery.execute(queryStr);
+
+			// group already exist
+			if(rset == null || rset.getRow() == 0) {
+				return null;
+			}
+			//if there's a rset
+			else {
+				rset.next();
+				//second column is lastname, third is firstname
+				//so pass the column number accordingly to get the info about user
+				String resultLastName = rset.getString(2);
+				String resultFirstName = rset.getString(3);
+				String resultUsername = rset.getString(4);
+				String resultEmail = rset.getString(5);
+
+				return new User(resultFirstName, resultLastName, resultUsername, resultEmail, null);
+
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
--- app/src/main/java/com/rip/roomies/sql/SQLLogin.java
+++ app/src/main/java/com/rip/roomies/sql/SQLLogin.java
@@ -2,6 +2,8 @@ package com.rip.roomies.sql;
 
 import com.rip.roomies.models.User;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 /**
@@ -11,7 +13,40 @@ public class SQLLogin {
 	private static final Logger log = Logger.getLogger(SQLLogin.class.getName());
 
 	public static User login(User user) {
-		return null;
+
+		//declare the execution query code for TSQL to login
+		final String queryStr = "EXEC Login @username = '"+ user.getUsername() +
+				"', @password = '" + user.getPassword() + "'";
+
+		ResultSet rset = null;
+
+		try {
+			// get the result table from query execution through sql
+			rset = SQLQuery.execute(queryStr);
+
+			// either username doesn't exist or password incorrect
+			if(rset == null || rset.getRow() == 0) {
+				return null;
+			}
+			//if there's a rset
+			else {
+				//get the next row, kind of like scanner.nextLine()
+				rset.next();
+				//second column is lastname, third is firstname
+				//so pass the column number accordingly to get the info about user
+				String resultLastName = rset.getString(2);
+				String resultFirstName = rset.getString(3);
+				String resultUsername = rset.getString(4);
+				String resultEmail = rset.getString(5);
+
+				return new User(resultFirstName, resultLastName, resultUsername, resultEmail, null);
+
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public static boolean passRetrieve(User user) {
