d804430e8e78d5e8cbb8553bd7113b1d47897312
Aimee Wong <Aimee Wong>
2016-05-15 20:59:00 -0700

    Create Group XML

---
+++ app/src/main/java/com/rip/roomies/activities/groups/JoinCreateGroup.java
@@ -0,0 +1,7 @@
+package com.rip.roomies.activities.groups;
+
+/**
+ * Created by aimee on 5/15/2016.
+ */
+public class JoinCreateGroup {
+}
---
+++ app/src/main/java/com/rip/roomies/events/groups/JoinCreateGroupListener.java
@@ -0,0 +1,7 @@
+package com.rip.roomies.events.groups;
+
+/**
+ * Created by aimee on 5/15/2016.
+ */
+public class JoinCreateGroupListener {
+}
