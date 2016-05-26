1f9d88f9337e03b776930552c4d88bd8cd213eb7
Zacharie Doney <zdoney@ucsd.edu>
2016-05-25 19:46:22 -0700

    Clicking 'Overall Duties' now takes you to the duties screen

--- app/src/main/java/com/rip/roomies/activities/home/Home.java
+++ app/src/main/java/com/rip/roomies/activities/home/Home.java
@@ -1,9 +1,15 @@
 package com.rip.roomies.activities.home;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.rip.roomies.R;
 import com.rip.roomies.activities.GenericActivity;
+import com.rip.roomies.activities.duties.CreateDuty;
+import com.rip.roomies.activities.duties.ListAllDuties;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -12,9 +18,22 @@ import com.rip.roomies.activities.GenericActivity;
 public class Home extends GenericActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		TextView dutiesScreen;
+
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_home);
+
+		dutiesScreen = (TextView) findViewById(R.id.home_overallduties);
+
+		final Activity self = this;
+
+		dutiesScreen.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(self, ListAllDuties.class));
+			}
+		});
 	}
 
 	@Override
