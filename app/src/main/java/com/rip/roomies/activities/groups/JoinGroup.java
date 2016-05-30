feaa3a5a1b34b3dd4ec9f70dfc1a3473094e87cf
Michael Zhong <m2zhong@ucsd.edu>
2016-05-30 00:16:09 -0700

    fixed more conflicts

--- app/src/main/java/com/rip/roomies/activities/groups/JoinGroup.java
+++ app/src/main/java/com/rip/roomies/activities/groups/JoinGroup.java
@@ -16,11 +16,8 @@ public class JoinGroup extends GenericActivity {
 		setContentView(R.layout.activity_join_group);
 
 		EditText name = (EditText) findViewById(R.id.group_name);
-<<<<<<< HEAD
+
 		Button submit = (Button) findViewById(R.id.join_group_btn);
-=======
-		Button submit = (Button) findViewById(R.id.join_group_submit);
->>>>>>> 2a34d35dc132dc2b07ba22bb70c722fc0e194030
 
 		submit.setOnClickListener(new JoinGroupListener(this, name));
 	}
