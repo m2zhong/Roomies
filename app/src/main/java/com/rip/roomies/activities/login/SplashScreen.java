bc7693497404c291b818597c1925d0559230949c
Hao Tu <haotuusa@gmail.com>
2016-06-04 05:18:01 -0700

    put the splash screen when user waiting for auto login instead of black screen

--- app/src/main/java/com/rip/roomies/activities/login/SplashScreen.java
+++ app/src/main/java/com/rip/roomies/activities/login/SplashScreen.java
@@ -1,17 +1,15 @@
 package com.rip.roomies.activities.login;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Display;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.rip.roomies.R;
 import com.rip.roomies.activities.GenericActivity;
-import com.rip.roomies.activities.groups.GroupChoice;
 import com.rip.roomies.application.SaveSharedPreference;
 import com.rip.roomies.controllers.LoginController;
 import com.rip.roomies.functions.LoginFunction;
@@ -28,20 +26,16 @@ public class SplashScreen extends GenericActivity implements LoginFunction{
 	private static final Logger log = Logger.getLogger(SplashScreen.class.getName());
 	private static final double IMAGE_WIDTH_RATIO = 3.0 / 10;
 	private static final double IMAGE_HEIGHT_RATIO = 2.0 / 25;
-
+	private Activity activity;
+	private LoginFunction func;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		String username = SaveSharedPreference.getUsername(this);
-		String password = SaveSharedPreference.getPassword(this);
-		if(username.length() != 0 && password.length() != 0) {
-			LoginController.getController().login(this, SaveSharedPreference.getUsername(this),
-					SaveSharedPreference.getPassword(this));
-			return;
-		}
 
 		setContentView(R.layout.activity_splash_screen);
 
+		activity = this;
+		func = this;
 		LoginController.getController().connect();
 
 		log.info(String.format(Locale.US, InfoStrings.SWITCH_ACTIVITY_DELAYED,
@@ -51,7 +45,15 @@ public class SplashScreen extends GenericActivity implements LoginFunction{
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				toLogin();
+				String username = SaveSharedPreference.getUsername(activity);
+				String password = SaveSharedPreference.getPassword(activity);
+				if(username.length() != 0 && password.length() != 0) {
+					LoginController.getController().login(func, SaveSharedPreference.getUsername(activity),
+							SaveSharedPreference.getPassword(activity));
+				}
+				else {
+					toLogin();
+				}
 			}
 		}, DisplayStrings.TOAST_LONG_LENGTH);
 
@@ -65,8 +67,6 @@ public class SplashScreen extends GenericActivity implements LoginFunction{
 
 
 	}
-
-
 	@Override
 	public void loginFail() {
 		Toast.makeText(this, DisplayStrings.LOGIN_FAIL, Toast.LENGTH_LONG).show();
@@ -75,9 +75,11 @@ public class SplashScreen extends GenericActivity implements LoginFunction{
 	@Override
 	public void loginSuccess(User user) {
 		if (Group.getActiveGroup() == null) {
-			this.startActivity(new Intent(this, GroupChoice.class));
+			toGroupChoice();
 		}
 		else {
-			this.toHome();
-		}	}
+			toHome();
+		}
+	}
+
 }
